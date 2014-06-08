8b5d0582c1f2b065e5eef1d7dcecfe47589c7dcc
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2014-06-08 07:02:38 +0530

    Alterd

--- songSubtitleDownloader.py
+++ songSubtitleDownloader.py
@@ -33,38 +33,58 @@ def getCmd():
     else:
         sys.exit("Command Error:" + Help())
 
-def lyricsMint(song, save):
+def lyricsMint(song, album, save):
     print song, "Searching..."
+    # Searching Song
     url = "http://www.lyricsmint.com/search?q="+"+".join(song.split())
     req = urllib2.Request(url=url)
     html = urllib2.urlopen(req)
     soup = BeautifulSoup(html)
     soup = soup.select('.blog-posts.hfeed .date-outer')
+
+    #No Lyrics
     if soup.__len__() == 0:
         print "No Lyrics found"
         return
     lyric = None
     href = []
     song = ''.join(ch for ch in song if ch.isalnum() or ch.isspace())
+    album = ''.join(ch for ch in album if ch.isalnum() or ch.isspace())
+
+    #Searching require lyric
     for s in soup:
         temp = s.select('.post-title a')[0]
         for name in song.split():
             if name.lower() in temp.text.lower():
                 href.append((temp.text, temp['href']))
                 break
-    if href == []:
-        for i in range(soup.__len__()):
-            title = soup[i].select('.post-title a')[0]
-            print i, title.text
-        choice = input("Enter the choice: ")
-        lyric = (soup[choice].select('.post-title a')[0].text, soup[choice].select('.post-title a')[0]['href'])
-    elif href.__len__() == 1:
+    
+    if href.__len__() == 1:
         lyric = href[0]
     else:
+        if href.__len__() == 0:
+            for i in range(soup.__len__()):
+                title = soup[i].select('.post-title a')[0]
+                href.append((title.text, title['href']))
+                #print i, title.text
+            #choice = input("Enter the choice: ")
+            #lyric = (soup[choice].select('.post-title a')[0].text, soup[choice].select('.post-title a')[0]['href'])
+        else:
+            pass
+
+        temp = []
         for i in range(href.__len__()):
-            print i, href[i][0]
-        choice = input("Enter the choice: ")
-        lyric = href[choice]
+            for name in album.split():
+                if name.lower() in href[i][0].lower():
+                    temp.append(href[i])
+                    break
+        if temp.__len__() == 1:
+            lyric = temp[0]
+        else:
+            for i in range(temp.__len__()):
+                print i, temp[i][0]
+            choice = input("Enter the choice: ")
+            lyric = temp[choice]
     
     url = lyric[1]
     req = urllib2.Request(url=url)
@@ -74,7 +94,7 @@ def lyricsMint(song, save):
     file = open(save, 'w')
     file.write(soup[0].select('p')[0].text)
     file.close()
-    print 'Downloaded'
+    print 'Downloaded!'
     
 sys.argv.pop(0)
 
@@ -88,7 +108,7 @@ while sys.argv.__len__() != 0:
         songName = mut['TIT2'].text[0]
         albumName = mut['TALB'].text[0]
         saveAs = source.rsplit('.', 1)[0] + '.srt'
-        lyricsMint(songName, saveAs)
+        lyricsMint(songName, albumName, saveAs)
     
     # For a folder
     if instruction in ('-d', '--directory'):
@@ -100,5 +120,5 @@ while sys.argv.__len__() != 0:
                 songName = mut['TIT2'].text[0]
                 albumName = mut['TALB'].text[0]
                 saveAs = (dirPath+f).rsplit('.', 1)[0] + '.srt'
-                lyricsMint(songName, saveAs)
+                lyricsMint(songName, albumName, saveAs)
                 print "Done"
