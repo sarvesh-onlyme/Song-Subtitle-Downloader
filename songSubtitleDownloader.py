1b7d4cdfcf379b0f264200020c689a64895fd6f3
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2014-06-08 05:53:12 +0530

    Altered script

--- songSubtitleDownloader.py
+++ songSubtitleDownloader.py
@@ -21,13 +21,10 @@ def getCmd():
             return '-h', None
 
     # Return first most instruction, source
-    print sys.argv
     temp = sys.argv.pop(0)
-    print temp
     if temp in ('-i', '--input', '-d', '--directory'):
         instruction = temp
     else:
-        print "#"
         sys.exit("Command Error:" + Help())
 
     temp = sys.argv.pop(0)
@@ -37,17 +34,21 @@ def getCmd():
         sys.exit("Command Error:" + Help())
 
 def lyricsMint(song, save):
+    print song, "Searching..."
     url = "http://www.lyricsmint.com/search?q="+"+".join(song.split())
     req = urllib2.Request(url=url)
     html = urllib2.urlopen(req)
     soup = BeautifulSoup(html)
     soup = soup.select('.blog-posts.hfeed .date-outer')
-
+    if soup.__len__() == 0:
+        print "No Lyrics found"
+        return
     lyric = None
     href = []
+    song = ''.join(ch for ch in song if ch.isalnum() or ch.isspace())
     for s in soup:
         temp = s.select('.post-title a')[0]
-        for name in songName.split():
+        for name in song.split():
             if name.lower() in temp.text.lower():
                 href.append((temp.text, temp['href']))
                 break
@@ -73,30 +74,31 @@ def lyricsMint(song, save):
     file = open(save, 'w')
     file.write(soup[0].select('p')[0].text)
     file.close()
-
+    print 'Downloaded'
+    
 sys.argv.pop(0)
 
 while sys.argv.__len__() != 0:
     instruction, source = getCmd()
-    print source
     source = source.replace('\\', '')
 
     # For one file
     if instruction in ('-i', '--input'):
-        print source
         mut = MP3(source)
         songName = mut['TIT2'].text[0]
         albumName = mut['TALB'].text[0]
         saveAs = source.rsplit('.', 1)[0] + '.srt'
-        lyricsMint(songName+albumName, saveAs)
+        lyricsMint(songName, saveAs)
     
     # For a folder
     if instruction in ('-d', '--directory'):
         for (dirPath, dirName, fileName) in walk(source):
             for f in fileName:
-                mut = MP3(dirPath+'/'+f)
+                if not f.rsplit('.', 1)[-1] == 'mp3':
+                    continue
+                mut = MP3(dirPath+f)
                 songName = mut['TIT2'].text[0]
                 albumName = mut['TALB'].text[0]
                 saveAs = (dirPath+f).rsplit('.', 1)[0] + '.srt'
-                print songName
-                lyricsMint(songName+albumName, saveAs)
+                lyricsMint(songName, saveAs)
+                print "Done"
